374857313986600ee2b0867fa1a7ec6dc41a5686
Marrin27b <martinmonsalve27b@gmail.com>
2025-04-23 09:11:45 -0500

    Add files via upload

---
+++ Codigo parcial c++ <iostream>.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int temperatura[7];
+    for(int i = 0; i < 7; i++){
+        cout << "Ingrese la temperatura: " << i + 1 << " : " << endl;
+
+        cin >> temperatura[i];
+    }
+
+    int max = 35
+    int max_dias;
+    for (int j = 0; j < temperatura; j++)
+    {
+        if (temperatura[j] > max){
+            max = temperatura[j]
+        }
+        
+    }
+    
